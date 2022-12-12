7783adae1db2caf4184c7fa1601afdcbd6fc852f
Minsu <jminsu084@gmail.com>
2022-12-13 02:14:01 +0900

    UDP 서버-클라이언트 예제

--- ex5/udp_client.c
+++ ex5/udp_client.c
@@ -1,6 +1,6 @@
 /*
  * 시스템 프로그래밍 lab4 과제 실습 5번
- * 파일 이름: tcp_client.c
+ * 파일 이름: udp_client.c
  * 개발자 : 20183152 정민수
  */
 
@@ -16,53 +16,50 @@
 
 #define PORT 4200 /* 서버 포트 */
 #define MAXDATASIZE 100
+
 int main(int argc, char *argv[])
 {
-    int csock, numbytes;
+    int csock;
     char buf[MAXDATASIZE];
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in to_addr;
+    struct sockaddr_in from_addr;
+    int from_len;
     int len;
+    int recv_len;
     if (argc != 3)
     {
-        fprintf(stderr, "usage: tcp_client <SERVER IP> < ECHO STRING > \n ");
+        fprintf(stderr, "usage: udp_client <SERVER IP> < ECHO STRING > \n ");
         exit(1);
     }
-    
     /* 클라이언트 소켓 생성 */
-    if ((csock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    if ((csock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
         perror("socket");
         exit(1);
     }
-
     /* 서버 주소 설정 */
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(PORT);
-
-    /* 서버 연결 */
-    if (connect(csock, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) == -1)
-    {
-        perror("connect");
-        exit(1);
-    }
-
-    /* 메시지 송수신 */
+    memset(&to_addr, 0, sizeof(to_addr));
+    to_addr.sin_family = AF_INET;
+    to_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    to_addr.sin_port = htons(PORT);
+    /* 메시지 송신 */
     memset(buf, 0, MAXDATASIZE);
     strcpy(buf, argv[2]);
     len = strlen(buf);
-    if (send(csock, buf, len, 0) != len)
+    if (sendto(csock, buf, len, 0, (struct sockaddr *)&to_addr, sizeof(to_addr)) != len)
     {
         fprintf(stderr, "send failed...\n");
         exit(1);
     }
+    /* 메시지 수신 */
     memset(buf, 0, MAXDATASIZE);
-    if ((numbytes = recv(csock, buf, MAXDATASIZE, 0)) == -1)
+    from_len = sizeof(from_addr);
+    if ((recv_len = recvfrom(csock, buf, MAXDATASIZE, 0, (struct sockaddr *)&from_addr, &from_len)) == -1)
     {
         perror("recv");
         exit(1);
     }
     printf("Received: %s\n", buf);
+    /* 연결 종료 */
     close(csock);
 }
